a860734f0bc484dce0822c7aa0d10391930be810
Evgeny Kuznetsov <umtius@gmail.com>
2022-05-04 17:04:55 +0500

    feat: add UserRepository.java
    The UserRepository.java interface contains CRUD functional to handle queries to the database to getting entities of type "User".

---
+++ src/main/java/com/evgKuznetsov/expert/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.evgKuznetsov.expert.repository;
+
+import com.evgKuznetsov.expert.model.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+@Transactional(readOnly = true, propagation = Propagation.REQUIRED)
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("SELECT usr FROM user usr LEFT JOIN FETCH usr.roles")
+    List<User> findAll();
+}
