2ab2d041cfb15d36317cc0f9112eb1292f3fa299
Evgeny Kuznetsov <umtius@gmail.com>
2022-06-09 09:41:43 +0500

    refactoring[project]: delete garbage code

--- src/main/java/com/evgKuznetsov/expert/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.evgKuznetsov.expert.repository;
-
-import com.evgKuznetsov.expert.model.entities.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Integer> {
-}
