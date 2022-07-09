6f0dcf8a474af7c9062c81cf3f67c374c11001a2
Evgeny Kuznetsov <umtius@gmail.com>
2022-07-09 16:21:25 +0500

    fix[RoleRepository.java] - fix the annotations

--- src/main/java/com/evgKuznetsov/expert/repository/RoleRepository.java
+++ src/main/java/com/evgKuznetsov/expert/repository/RoleRepository.java
@@ -2,7 +2,12 @@ package com.evgKuznetsov.expert.repository;
 
 import com.evgKuznetsov.expert.model.entities.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
+@Repository
+@Transactional(readOnly = true, propagation = Propagation.REQUIRED)
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
 }
